f63a08637e42492be683c3b35247a89c60dca595
Natanael Lima <taelima1997@gmail.com>
2021-01-29 14:00:00 +0100

    create dynamics static pages

---
+++ src/pages/categories/[slug].tsx
@@ -0,0 +1,81 @@
+import SEO from "@/components/Description/SEO";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { Container, List, Title } from "../../styles/pages/Home";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+interface Product {
+  id: number;
+  title: String;
+  price: number;
+}
+
+interface Category {
+  id: Number;
+  title: String;
+}
+interface Homeprops {
+  products: Product[];
+}
+
+export default function categories({ products }: Homeprops) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <p>Gerando pagina estática...</p>;
+  }
+
+  return (
+    <Container>
+      <SEO title="Top 10 produtos!" image="embed.jpg" />
+
+      <Title>NEXT JS⚡⚡</Title>
+
+      <h1>Produtos</h1>
+
+      <List>
+        {products.length > 0 ? (
+          products.map((p) => (
+            <li key={p.id}>
+              <Link href={`catalog/product/${p.id}`}>
+                <a>{p.title}</a>
+              </Link>
+            </li>
+          ))
+        ) : (
+          <p>Nenhum produto para essa categoria {`:(`} </p>
+        )}
+      </List>
+    </Container>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const categories = await (
+    await fetch(`http://localhost:3333/categories`)
+  ).json();
+
+  const paths = categories.map((c: Category) => ({
+    params: { slug: c.id },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<Homeprops> = async ({ params }) => {
+  const { slug } = params;
+
+  const products = await (
+    await fetch(`http://localhost:3333/products?category_id=${slug}`)
+  ).json();
+
+  return {
+    revalidate: 5,
+    props: {
+      products,
+    },
+  };
+};
